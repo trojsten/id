cae93811dca97b45e9c94c864b5f0ecece5af4c8
Adam Zahradník <adam@zahradnik.xyz>
2023-05-21 23:56:11 +0200

    settings: add form for our profile data

--- trojstenid/users/templatetags/settings.py
+++ trojstenid/users/templatetags/settings.py
@@ -6,6 +6,7 @@ register = template.Library()
 @register.inclusion_tag("_partials/navbar_items.html", takes_context=True)
 def navbar_menu(context):
     items = [
+        ("mdi:user", "Osobné údaje", "account_profile"),
         ("mdi:email", "E-mailové adresy", "account_email"),
         ("mdi:password", "Zmena hesla", "account_change_password"),
         ("mdi:account-key", "Externé účty", "socialaccount_connections"),
