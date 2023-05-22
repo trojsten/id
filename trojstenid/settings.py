a457b1e7c040b27b072e54477f6e02249bdf5459
Adam Zahradník <adam@zahradnik.xyz>
2023-05-22 11:28:40 +0200

    provider: load oidc key from environment

--- trojstenid/settings.py
+++ trojstenid/settings.py
@@ -149,14 +149,15 @@ SOCIALACCOUNT_FORMS = {
 RECAPTCHA_PUBLIC_KEY = env("RECAPTCHA_PUBLIC")
 RECAPTCHA_PRIVATE_KEY = env("RECAPTCHA_PRIVATE")
 
-with open(BASE_DIR / "oidc.key") as f:
-    oidc_key = f.read()
-
 OAUTH2_PROVIDER_APPLICATION_MODEL = "users.Application"
 OAUTH2_PROVIDER = {
     "OAUTH2_VALIDATOR_CLASS": "trojstenid.users.validators.OurOAuth2Validator",
     "OIDC_ENABLED": True,
-    "OIDC_RSA_PRIVATE_KEY": oidc_key,
+    "OIDC_RSA_PRIVATE_KEY": (
+        "-----BEGIN PRIVATE KEY-----\n"
+        f"{env('OIDC_KEY')}\n"
+        "-----END PRIVATE KEY-----"
+    ),
     "SCOPES": {
         "openid": "OpenID Connect",
         "profile": "Základné osobné údaje",
