aa47f8d1d8d560c67d7baf46cb696b9d58ba76f0
Adam Zahradník <adam@zahradnik.xyz>
2024-06-03 12:31:44 +0200

    assign veduci group after email confirmation

--- trojstenid/users/signals.py
+++ trojstenid/users/signals.py
@@ -1,5 +1,5 @@
 from allauth.account.models import EmailAddress
-from allauth.account.signals import user_logged_in
+from allauth.account.signals import email_confirmed, user_logged_in
 from django.contrib.auth.models import Group
 from django.dispatch import receiver
 
@@ -7,10 +7,17 @@ from trojstenid.users.models import User
 
 
 @receiver(user_logged_in)
-def assign_groups(request, user: User, **kwargs):
+def assign_groups_after_login(request, user: User, **kwargs):
     has_trojsten_mail = EmailAddress.objects.filter(
         user=user, email__endswith="@trojsten.sk", verified=True
     ).exists()
     if has_trojsten_mail:
         veduci, _ = Group.objects.get_or_create(name="trojsten:veduci")
         user.groups.add(veduci)
+
+
+@receiver(email_confirmed)
+def assign_groups_after_confirm(request, email: EmailAddress, **kwargs):
+    if email.email.endswith("@trojsten.sk"):
+        veduci, _ = Group.objects.get_or_create(name="trojsten:veduci")
+        email.user.groups.add(veduci)
