181e08852ae66650585b43d258441d81dc851ca9
Adam Zahradník <adam@zahradnik.xyz>
2026-05-31 21:50:24 +0200

    Apply suggestions from code review
    Co-authored-by: Andrej Lackovič <andrej.lackovic@drobcekovia.sk>

--- trojstenid/users/urls_groups.py
+++ trojstenid/users/urls_groups.py
@@ -3,6 +3,6 @@ from django.urls import path
 from trojstenid.users.views import GroupListView, GroupSyncView
 
 urlpatterns = [
-    path("groups/", GroupListView.as_view(), name="group_list"),
-    path("groups/sync/", GroupSyncView.as_view(), name="group_sync"),
+    path("", GroupListView.as_view(), name="group_list"),
+    path("sync/", GroupSyncView.as_view(), name="group_sync"),
 ]
