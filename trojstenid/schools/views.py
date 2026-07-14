f68f58ed8b878eb78496edbecf0ae8e5de2e47a9
Adam Zahradník <adam@zahradnik.xyz>
2026-07-14 15:39:07 +0200

    add school confirmation dialog

--- trojstenid/schools/views.py
+++ trojstenid/schools/views.py
@@ -6,6 +6,7 @@ from django.db import transaction
 from django.db.models import QuerySet
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.functional import cached_property
 from django.views.generic import FormView, ListView, TemplateView
 
@@ -49,23 +50,31 @@ class SchoolRecordCreateView(LoginRequiredMixin, FormView):
         ctx["school"] = self.school
         return ctx
 
-    @transaction.atomic
-    def form_valid(self, form: SchoolRecordForm):  # pyright: ignore
-        assert isinstance(self.request.user, User)
-
+    def build_record(self, form: SchoolRecordForm) -> UserSchoolRecord:
         type_id, year = form.cleaned_data["start_year"].split(":")
-        start_date = form.cleaned_data["start_date"]
-        end_date = form.cleaned_data["end_date"]
-
-        UserSchoolRecord.objects.end_active(self.request.user, start_date)
-        record = UserSchoolRecord(
+        return UserSchoolRecord(
             user=self.request.user,
             school=self.school,
-            start_date=start_date,
-            end_date=end_date,
+            start_date=form.cleaned_data["start_date"],
+            end_date=form.cleaned_data["end_date"],
             school_type_id=type_id,
-            start_year=year,
+            start_year=int(year),
         )
+
+    def form_valid(self, form: SchoolRecordForm):  # pyright: ignore
+        record = self.build_record(form)
+        if not self.request.POST.get("confirmed"):
+            ctx = self.get_context_data(form=form)
+            ctx["confirmation"] = True
+            ctx["current_year"] = record.get_current_year_display(timezone.now().date())
+            return self.render_to_response(ctx)
+
+        return self.save_record(form, record)
+
+    @transaction.atomic
+    def save_record(self, form: SchoolRecordForm, record: UserSchoolRecord):
+        assert isinstance(self.request.user, User)
+        UserSchoolRecord.objects.end_active(self.request.user, record.start_date)
         try:
             record.full_clean()
         except ValidationError as e:
