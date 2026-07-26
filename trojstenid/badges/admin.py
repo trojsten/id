096e3672e9dce012ce1125235603c91d7bfa98ae
Andrej Lackovič <andrej.lackovic@trojsten.sk>
2026-07-26 14:08:02 +0200

    perf: make badge admin usable (#108)
    Co-authored-by: Jakub Šiagi <jakub.siagi@trojsten.sk>

--- trojstenid/badges/admin.py
+++ trojstenid/badges/admin.py
@@ -5,6 +5,11 @@ from trojstenid.badges.models import Badge, BadgeAssignment, BadgeGroup
 
 class BadgeAssignmentInline(admin.TabularInline):
     model = BadgeAssignment
+    autocomplete_fields = ["user"]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related("badge", "user")
 
 
 @admin.register(Badge)
